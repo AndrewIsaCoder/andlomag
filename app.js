158f6fee5cc45d621be747f3849a378668d7c91c
Stoian Andrei <stoian.andrei239@gmail.com>
2025-10-14 18:25:19 +0300

    feat: enhance admin functionality with product management features

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-window.addEventListener("DOMContentLoaded", displayProducts);
+window.addEventListener('DOMContentLoaded', displayProducts);
 
-const URL = "https://68e3ed6d8e116898997a7740.mockapi.io/products";
+const URL = 'https://68e3ed6d8e116898997a7740.mockapi.io/products';
 
 function displayProducts() {
-  fetch(URL)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network error!");
-      }
-      return response.json();
-    })
-    .then((products) => {
-      const container = document.querySelector(".products-container");
-      container.innerHTML = products.map(product => `
-        <div class="product-card">
-          <img src="${product.imageURL}" alt="Product image">
-          <div class="product-info">
-            <h3>${product.name}</h3>
-            <div class="price">${product.price} Ron</div>
-            <div class="buttons">
-              <button class="details-btn">Details</button>
-              <button class="cart-btn">Add to cart</button>
-            </div>
-          </div>
-        </div>
-      `).join('');
-    })
-    .catch(error => {
-      console.error(error);
-    });
+	fetch(URL)
+		.then((response) => {
+			if (response.ok === false) {
+				throw new Error('Networn error!');
+			} else {
+				return response.json();
+			}
+		})
+		.then(
+			(products) =>
+				(document.querySelector('.products-container').innerHTML = products
+					.map(
+						(product) => `
+         <div class="product-card">
+				<img
+					src=${product.imageURL}
+					alt="Product image"
+				/>
+				<div class="product-info">
+					<h3>${product.name}</h3>
+					<div class="price">${product.price} LEI</div>
+					<div class="buttons">
+						<button class="details-btn">Details</button>
+						<button class="cart-btn">Add to Cart</button>
+					</div>
+				</div>
+			</div>   
+      `
+					)
+					.join(''))
+		);
 }
